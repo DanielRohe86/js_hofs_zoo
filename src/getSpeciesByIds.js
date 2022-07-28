4e3794bc4e95f22b2ffb1ad990e77425d2fcfba5
DanielRohe86 <psicologo.rohe@gmail.com>
2022-07-28 17:18:54 -0300

    adiciona requisito 1, que funciona, mas não após passar no linter

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,43 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const arrayOfSpecies = { species }.species;
+
 function getSpeciesByIds(ids) {
-  // seu código aqui
+
+  const idAnimals = arrayOfSpecies.map((ids) => {
+    let newArray;
+    newArray = ids.id});
+    
+    const idNames = arrayOfSpecies.map((ids) => {
+      let newArray;
+      newArray = ids.names
+      return newArray});
+
+  if (idAnimals === [ ]) {
+    return [];
+  }
+     return idNames;   
 }
 
-module.exports = getSpeciesByIds;
+// TENTATIVA 1
+// const result = arrayOfSpecies.map((element) => {
+//   let newArray = Object.values(element.name).join(", ");
+//   return newArray;
+// })
+
+// //RETORNO DO TIPO:
+// [
+//   'l, i, o, n, s',
+
+// TENTATIVA 2 FUNCIONA!!!!!!!!!!
+
+// const result = arrayOfSpecies.map((element) => newArray = element.name);
+
+// console.log(species[0].name);
+// console.log(data)
+// console.log(arrayOfSpecies[0])
+// console.log(result); FUNCIONA
+console.log(getSpeciesByIds()) /* FUNCIONA, MAS NÃO QUANDO PASSA PARA DENTRO DA FUNÇÃO COM RETORNO INTERNO - FUNCIONA EM UMA LINHA, MAS NÃO PASSA NO LINTER */
+
+// module.exports = getSpeciesByIds;

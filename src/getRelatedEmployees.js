60ffac0fcd8c6ab7aa6a0d2aa2b20623d866f334
DanielRohe86 <psicologo.rohe@gmail.com>
2022-07-29 19:24:14 -0300

    adiciona module.exports ao requisito 4 e adiciona requisito 5 parcialmente feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -84,3 +84,5 @@ function getRelatedEmployees(managerId) {
 // // console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 // console.log(filteredManagers);
+
+module.exports = { isManager, getRelatedEmployees };

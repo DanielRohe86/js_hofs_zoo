44764ee52b0d6872d0b6dc596aa6b9339859efd0
DanielRohe86 <psicologo.rohe@gmail.com>
2022-07-31 14:19:36 -0300

    adiciona requisitos 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,35 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('test 1 - typeof', () => {
+    expect(typeof handlerElephants).toEqual('function');
+  });
+
+  it('test 2 - undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+
+  it('test 3 - not a string ', () => {
+    expect(handlerElephants(5)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('test 4 - retorna chave pedida', () => {
+    expect(handlerElephants('name')).toBe('elephants');
+  });
+
+  it('test 5 - retorna valor da callback do caso count', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+
+  it('test 6 - retorna valor da callback do caso names', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+
+  it('test 7 - retorna valor da callback do caso averageAge', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+
+    // it('test 8 - ', () => {
+    
+  // });
+});

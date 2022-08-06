fc0d981c72e65701de4423647c627d02e2c32f2e
DanielRohe86 <psicologo.rohe@gmail.com>
2022-08-06 18:08:20 -0300

    adiciona requisito 9, faltando testagem passar quando a função não recebe parâmetro, ou não recebe parâmetro que seja um dia ou um animal

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,10 +1,9 @@
 const data = require('../data/zoo_data');
 
-
 const zooSpecies = data.species;
 const mappedSpecies = zooSpecies.map((element) => element.name);
+const zooDays = Object.keys(data.hours);
 
-// const zooDays = Object.keys(data.hours);
 // console.log(zooDays);
 // const reqTwo = zooDays.forEach((item) => {
 //   const object = {
@@ -27,16 +26,16 @@ const mappedSpecies = zooSpecies.map((element) => element.name);
 
 // console.log(obj);
 
-/* RETORNA
-{
-  Tuesday: { open: 8, close: 6 },
-  Wednesday: { open: 8, close: 6 },
-  Thursday: { open: 10, close: 8 },
-  Friday: { open: 10, close: 8 },
-  Saturday: { open: 8, close: 10 },
-  Sunday: { open: 8, close: 8 },
-  Monday: { open: 0, close: 0 }
-}*/
+// RETORNA:
+// {
+//   Tuesday: { open: 8, close: 6 },
+//   Wednesday: { open: 8, close: 6 },
+//   Thursday: { open: 10, close: 8 },
+//   Friday: { open: 10, close: 8 },
+//   Saturday: { open: 8, close: 10 },
+//   Sunday: { open: 8, close: 8 },
+//   Monday: { open: 0, close: 0 }
+// }
 
 // FORMATAÇÃO DA PARTE 2
 /* 'Tuesday': {
@@ -87,20 +86,70 @@ const mappedSpecies = zooSpecies.map((element) => element.name);
 
 // module.exports = getSchedule;
 
+//  RESPOSTA
+/* function getSchedule(scheduleTarget) {
 
-//RESPOSTA
-function getSchedule(scheduleTarget) {
-  if (mappedSpecies.includes(scheduleTarget)) {
-    const targetAnimal = zooSpecies.filter((animal) => (animal.name == scheduleTarget));
-    return targetAnimal[0].availability;
   }
   // if (!scheduleTarget || !zooDays.includes(scheduleTarget)) {
-  //   return 
+  //   return
 
+  } */
+
+// CALLBACKS
+
+const getAnimalinThatDay = (scheduleTarget) => {
+  const targetAnimal = zooSpecies.filter((animal) => (animal.name === scheduleTarget));
+  return targetAnimal[0].availability;
+};
+
+const getScheduleHoursAnimal = (scheduleTarget) => {
+  const hours = data.hours[scheduleTarget];
+  const dayAnimal = zooDays.find((element) => element === scheduleTarget);
+  if (dayAnimal === 'Monday') {
+    return { [scheduleTarget]: {
+      officeHour: 'CLOSED', exhibition: 'The zoo will be closed!',
+    } };
   }
 
+  const animalsOfDay = (scheduleTarget) => {
+    const animalsByDay = zooSpecies
+      .filter((specie) => specie.availability.includes(scheduleTarget))
+      .map((animal) => animal.name);
+    return animalsByDay;
+  };
 
-console.log(getSchedule('lions'));
+  return { [scheduleTarget]: {
+    officeHour: `Open from ${hours.open}am until ${hours.close}pm`,
+    exhibition: animalsOfDay(scheduleTarget),
+  } };
+};
 
+const fullSchedule = () => {
+  zooDays.reduce((acc, curr) => ({ ...acc, ...getScheduleHoursAnimal(curr) }), {});
+};
+
+// RESPOSTA FINAL
+
+function getSchedule(scheduleTarget) {
+  if (!scheduleTarget) {
+    return fullSchedule();
+  }
+
+  if (zooDays.includes(scheduleTarget)) {
+    return getScheduleHoursAnimal(scheduleTarget);
+  }
+
+  if (mappedSpecies.includes(scheduleTarget)) {
+    return getAnimalinThatDay(scheduleTarget);
+  }
+
+  if (!data.species.find((element) => element.name === scheduleTarget)) {
+    return fullSchedule();
+  }
+}
+
+// console.log(getScheduleAnimalDay);
+
+// console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
